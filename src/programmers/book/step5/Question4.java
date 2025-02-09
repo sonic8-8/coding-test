5f1f10934defd60257ebdcc9cb7bbefbf1414766
이승환 <seaung13@gmail.com>
2025-02-10 03:55:00 +0900

    feat: 코딩 테스트 합격자 되기 자바편, 5장 배열 - 모의 테스트 4번 추가

---
+++ src/programmers/book/step5/Question4.java
@@ -0,0 +1,38 @@
+package programmers.book.step5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Question4 {
+    public int[] solution(int[] answers) {
+
+        // 프로그래머스 모의고사
+
+        int[][] patterns = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+
+        int[] scores = new int[3];
+
+        for (int i = 0; i < answers.length; i++) {
+            for (int j = 0; j < patterns.length; j++) {
+                if (answers[i] == patterns[j][i % patterns[j].length]) {
+                    scores[j]++;
+                }
+            }
+        }
+
+        int maxScore = Arrays.stream(scores).max().getAsInt();
+
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == maxScore) {
+                answer.add(i + 1);
+            }
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
