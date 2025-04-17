b24af494f6edc355f1bf7e23b5e0c158fc057683
이승환 <seaung13@gmail.com>
2025-04-18 04:42:26 +0900

    feat: 코딩 테스트 합격자 되기 자바편, 12장 백트래킹 - 몸풀기 문제 43번 추가

---
+++ src/programmers/book/step12/Question43.java
@@ -0,0 +1,33 @@
+package programmers.book.step12;
+
+import java.util.ArrayList;
+
+public class Question43 {
+
+    private ArrayList<ArrayList<Integer>> result;
+    private int n;
+
+    private void backtrack(int sum, ArrayList<Integer> selectedNums, int start) {
+        //정답 조건
+        if (sum == 10) {
+            result.add(selectedNums);
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            if (sum <= 10) {
+                ArrayList<Integer> list = new ArrayList<>(selectedNums);
+                list.add(i);
+                backtrack(sum + i, list, i + 1);
+            }
+        }
+    }
+
+    private ArrayList<ArrayList<Integer>> solution(int N) {
+        result = new ArrayList<>();
+        n = N;
+
+        backtrack(0, new ArrayList<>(), 1);
+        return result;
+    }
+}
