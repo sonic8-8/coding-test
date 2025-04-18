5e18493336edace1e7ead43815b3f29c90f506fc
이승환 <seaung13@gmail.com>
2025-04-19 02:56:29 +0900

    feat: 코딩 테스트 합격자 되기 자바편, 13장 정렬 - 모의 테스트 54번 추가

---
+++ src/programmers/book/step13/Question54.java
@@ -0,0 +1,42 @@
+package programmers.book.step13;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Question54 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] result = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0] - 1;
+            int end = commands[i][1];
+            int target = commands[i][2] - 1;
+
+            int[] processed = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(processed);
+            result[i] = processed[target];
+        }
+        return result;
+    }
+
+    public int[] solution2(int[] array, int[][] commands) {
+        int[] result = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0] - 1;
+            int end = commands[i][1] - 1;
+            int target = commands[i][2] - 1;
+
+            ArrayList<Integer> arr = new ArrayList<>();
+
+            for (int j = start; j <= end; j++) {
+                arr.add(array[j]);
+            }
+
+            Collections.sort(arr);
+            result[i] = arr.get(target);
+        }
+        return result;
+    }
+}
