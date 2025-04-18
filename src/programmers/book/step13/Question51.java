4fe7b3356c5dfd4f15b9a6ff28b8af522b8383e9
이승환 <seaung13@gmail.com>
2025-04-19 01:21:07 +0900

    feat: 코딩 테스트 합격자 되기 자바편, 13장 정렬 - 몸풀기 문제 51번 추가

---
+++ src/programmers/book/step13/Question51.java
@@ -0,0 +1,30 @@
+package programmers.book.step13;
+
+import java.util.*;
+
+public class Question51 {
+    private static int[] solution(int[] arr1, int[] arr2) {
+        //merge sort 구현
+        //쪼갠 다음 다시 합치면서 정렬
+        //todo: merge sort 구현
+
+        //sort 메서드 사용 연습
+        int[] merged = new int[arr1.length + arr2.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            merged[i] = arr1[i];
+        }
+
+        for (int i = arr1.length; i < arr1.length + arr2.length; i++) {
+            merged[i] = arr2[i - arr1.length];
+        }
+
+        Arrays.sort(merged);
+        return merged;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{1, 3, 5}, new int[]{2, 4, 6})));
+        System.out.println(Arrays.toString(solution(new int[]{1, 2, 3}, new int[]{4, 5, 6})));
+    }
+}
