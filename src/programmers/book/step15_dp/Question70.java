dd2993b99afcfd2aa61c0cb8e8676fe150980e3c
이승환 <seaung13@gmail.com>
2025-04-20 00:13:30 +0900

    feat: 코딩 테스트 합격자 되기 자바편, 15장 동적 계획법 - 모의 테스트 70번 추가

---
+++ src/programmers/book/step15_dp/Question70.java
@@ -0,0 +1,14 @@
+package programmers.book.step15_dp;
+
+public class Question70 {
+    public int solution(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        return dp[n];
+    }
+}
