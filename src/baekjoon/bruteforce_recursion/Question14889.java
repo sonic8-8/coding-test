e2214f9a06f2469eba2e5cb1b56eb7dae53c2015
이승환 <seaung13@gmail.com>
2025-05-12 23:17:46 +0900

    feat: 백준 14889번 스타트와 링크(브루트포스)

---
+++ src/baekjoon/bruteforce_recursion/Question14889.java
@@ -0,0 +1,65 @@
+package baekjoon.bruteforce_recursion;
+
+import java.util.*;
+import java.io.*;
+
+public class Question14889 {
+    static int N;
+    static int[][] board;
+    static boolean[] visited;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        board = new int[N + 1][N + 1];
+        visited = new boolean[N + 1];
+
+        for (int row = 1; row <= N; row++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int col = 1; col <= N; col++) {
+                board[row][col] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        backtrack(0, 1);
+
+        bw.write(String.valueOf(min));
+
+        br.close();
+        bw.close();
+    }
+
+    private static void backtrack(int depth, int start) {
+        if (depth == N / 2) {
+            calculateDiff();
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                backtrack(depth + 1, i + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    private static void calculateDiff() {
+        int teamA = 0;
+        int teamB = 0;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                if (visited[i] && visited[j]) {
+                    teamA += board[i][j] + board[j][i];
+                } else if (!visited[i] && !visited[j]) {
+                    teamB += board[i][j] + board[j][i];
+                }
+            }
+        }
+        min = Math.min(min, Math.abs(teamA - teamB));
+    }
+}
