7e2713e3a57f536ce4aa80b4f9a3c8e6c15cf8df
이승환 <seaung13@gmail.com>
2025-05-17 00:16:52 +0900

    feat: 백준 1934번 최소공배수(수학)

---
+++ src/baekjoon/math/Question1934.java
@@ -0,0 +1,40 @@
+package baekjoon.math;
+
+import java.util.*;
+import java.io.*;
+
+public class Question1934 {
+    static int T;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(lcm(a, b)).append("\n");
+        }
+
+        bw.write(sb.toString());
+
+        br.close();
+        bw.close();
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    private static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+}
