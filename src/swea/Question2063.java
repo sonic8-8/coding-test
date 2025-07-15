c8a0591a50dd44d77dd8a15fada1e8ea192cb181
이승환 <seaung13@gmail.com>
2025-07-15 09:10:13 +0900

    feat: SWEA 2063번 중간값 찾기

---
+++ src/swea/Question2063.java
@@ -0,0 +1,30 @@
+package swea;
+
+import java.io.*;
+import java.util.*;
+
+class Question2063 {
+    static int N;
+    static int[] array;
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        array = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(array);
+
+        bw.write(String.valueOf(array[N / 2]));
+
+        br.close();
+        bw.close();
+    }
+}
