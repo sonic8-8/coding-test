2cc47916c6f56fe1b4e2a7d330c263bf007c47f6
이승환 <seaung13@gmail.com>
2025-09-11 18:00:05 +0900

    feat: 백준 16236번 아기 상어 청킹 추가

---
+++ src/baekjoon/simulation/Main_16236_아기상어_청킹.java
@@ -0,0 +1,122 @@
+package baekjoon.simulation;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_16236_아기상어_청킹 {
+    static int N;
+    static int[][] board;
+
+    static final int[] DIR_ROW = {-1, 0, 1, 0};
+    static final int[] DIR_COL = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        board = new int[N][N];
+
+        Shark shark = null;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if (board[i][j] == 9) {
+                    shark = new Shark(i, j, 2, 0, 0);
+                    board[i][j] = 0;
+                }
+            }
+        }
+
+        bfs(shark);
+
+        bw.write(String.valueOf(shark.time));
+
+        br.close();
+        bw.close();
+    }
+
+    public static class Shark {
+        int row;
+        int col;
+        int size;
+        int eatCount;
+        int time;
+
+        public Shark(int row, int col, int size, int eatCount, int time) {
+            this.row = row;
+            this.col = col;
+            this.size = size;
+            this.eatCount = eatCount;
+            this.time = time;
+        }
+    }
+
+    private static void bfs(Shark shark) {
+        while (true) {
+            Queue<int[]> queue = new ArrayDeque<>(); // 방문 시뮬레이션 용 큐
+            boolean[][] visited = new boolean[N][N];
+
+            queue.add(new int[]{shark.row, shark.col, 0});
+            visited[shark.row][shark.col] = true;
+
+            List<int[]> candidates = new ArrayList<>(); // 실제로 방문할 위치
+            int minDist = Integer.MAX_VALUE;
+
+            while (!queue.isEmpty()) {
+                int[] current = queue.poll();
+                int row = current[0];
+                int col = current[1];
+                int dist = current[2];
+
+
+                if (dist > minDist) {
+                    break;
+                }
+
+                if (0 < board[row][col] && board[row][col] < shark.size) {
+                    candidates.add(new int[]{row, col, dist});
+                    minDist = dist;
+                }
+
+                for (int i = 0; i < 4; i++) {
+                    int nextRow = row + DIR_ROW[i];
+                    int nextCol = col + DIR_COL[i];
+
+                    if (nextRow < 0 || N <= nextRow || nextCol < 0 || N <= nextCol) {
+                        continue;
+                    }
+
+                    if (visited[nextRow][nextCol]) {
+                        continue;
+                    }
+
+                    if (board[nextRow][nextCol] <= shark.size) {
+                        queue.add(new int[]{nextRow, nextCol, dist + 1});
+                        visited[nextRow][nextCol] = true;
+                    }
+                }
+            }
+
+            if (candidates.isEmpty()) {
+                break;
+            }
+
+            candidates.sort((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+
+            int[] next = candidates.get(0);
+            shark.row = next[0];
+            shark.col = next[1];
+            shark.eatCount++;
+            shark.time += next[2];
+
+            if (shark.size == shark.eatCount) {
+                shark.size++;
+                shark.eatCount = 0;
+            }
+
+            board[next[0]][next[1]] = 0;;
+        }
+    }
+}
